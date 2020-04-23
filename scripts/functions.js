0bd3e53fad7f634725882372c7012ba9ef354017
lcwfl <lancecwilson@byui.edu>
2020-04-22 21:58:42 -0400

    new java

---
+++ scripts/functions.js
@@ -0,0 +1,7 @@
+var copyyear = modifieddate.getfullyear;
+
+function Index(){
+    document.getElementById("copy").innerHTML = copyyear;
+    document.getElementById("modified").innerHTML = "Last Updated: " + document.lastModified;
+
+};
