c838d800daa10957c90cfac94a9f5c612bd6b397
lcwfl <lancecwilson@byui.edu>
2020-05-17 00:32:26 -0400

    WK4

---
+++ lesson4/js/functions.js
@@ -0,0 +1,11 @@
+var d = new Date();
+var n = d.getFullYear();
+
+function Index(){
+    document.getElementById("copy").innerHTML = n;
+    document.getElementById("modified").innerHTML = "Last Updated: " + document.lastModified;
+
+};
+function toggleMenu() {
+    document.getElementsByClassName("navmenu")[0].classList.toggle("responsive");
+     }
