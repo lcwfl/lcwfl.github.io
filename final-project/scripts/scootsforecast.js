5d46d15ce718649eafabb537432955ecdfe7dd59
lcwfl <lancecwilson@byui.edu>
2020-07-30 08:11:45 -0400

    Purge old classes

--- final-project/scripts/scootsforecast.js
+++
@@ -1,8 +0,0 @@
-function getfivedays(){
-    var date = new Date();
-    date.setDate(date.getDate() + 5);
-    document.getElementById("fivedays").innerHTML = date.toISOString().split('T')[0];
-  }
-  
- 
-  
--- scripts/functions.js
+++
@@ -1,8 +0,0 @@
-var d = new Date();
-var n = d.getFullYear();
-
-function Index(){
-    document.getElementById("copy").innerHTML = n;
-    document.getElementById("modified").innerHTML = "Last Updated: " + document.lastModified;
-
-};
